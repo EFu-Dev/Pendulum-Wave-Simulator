f9533817a35c2b39b3c76ce76548c21274bf5ce5
EF <itsenyingf@gmail.com>
2026-05-12 16:31:44 -0500

    Refactor Pendulum class formatting and structure

--- Pendulum.java
+++ Pendulum.java
@@ -1,27 +1,26 @@
 public class Pendulum {
     double length; 
     double mass; 
-    double angle; // angle of the pendulum from the vertical (in radians)
+    double angle; 
     double angularVelocity; 
     double angularAcceleration; 
 
-    //constants for physics calculations
     private final double gravity = 9.8;
-    private final double damping = 0.995; //air resistance
+    private final double damping = 0.995; 
 
     public Pendulum(double l, double a){
         length = l;
-        angle = a; // initial angle
-        mass = 1.0; // default mass
-        angularVelocity = 0.0; // initial angular velocity
-        angularAcceleration = 0.0; // initial angular acceleration
+        angle = a;
+        mass = 1.0; 
+        angularVelocity = 0.0;
+        angularAcceleration = 0.0; 
     }
 
     public void update(double timeStep){
-        angularAcceleration = (-gravity / length) * Math.sin(angle); // a = -(g/L) * sin(theta)
-        angularVelocity += angularAcceleration * timeStep; // Euler Velocity
+        angularAcceleration = (-gravity / length) * Math.sin(angle);
+        angularVelocity += angularAcceleration * timeStep; 
         angularVelocity *= damping;
-        angle += angularVelocity * timeStep; // Euler Position
+        angle += angularVelocity * timeStep; 
     }
 }
 
